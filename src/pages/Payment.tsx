a333fd829885cd74f303171206d17f0f18d127e7
mahmoudbebo18 <mahmoudelkhateb18@gmail.com>
2025-04-27 17:40:19 +0300

    edit condition

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,37 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import LottieHandler from '../components/feedback/LottieHandler/LottieHandler';
+
 const PaymentFeedback = () => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
-    const status = queryParams.get("status");
-    const orderId = queryParams.get("order_id");
+    const success = queryParams.get("success") === "true"; 
+    const pending = queryParams.get("pending") === "true"; 
+    const orderId = queryParams.get("id");
 
     const [paymentStatus, setPaymentStatus] = useState<string>('');
     const [lottieType, setLottieType] = useState<"loading" | "success" | "error" | "pending" | "failed">("loading");
 
     useEffect(() => {
-        if (status) {
-            switch (status) {
-                case "pending":
-                    setPaymentStatus("Pending payment...");
-                    setLottieType("pending");
-                    break;
-                case "success":
-                    setPaymentStatus("Payment successful!");
-                    setLottieType("success");
-                    break;
-                case "failed":
-                    setPaymentStatus("Payment failed!");
-                    setLottieType("failed");
-                    break;
-                default:
-                    setPaymentStatus("We couldn't find your payment status.");
-                    setLottieType("error");
-                    break;
-            }
+        if (pending) {
+            setPaymentStatus("Pending payment...");
+            setLottieType("pending");
+        } else if (success) {
+            setPaymentStatus("Payment successful!");
+            setLottieType("success");
+        } else {
+            setPaymentStatus("Payment failed!");
+            setLottieType("failed");
         }
-    }, [status]);
+    }, [success, pending]);
 
     return (
         <div className="payment-feedback-container">
@@ -39,7 +31,7 @@ const PaymentFeedback = () => {
                 type={lottieType}
                 message={paymentStatus}
             />
-            <p>رقم الطلب: {orderId}</p>
+            <p> Order Number: {orderId}</p>
         </div>
     );
 };
